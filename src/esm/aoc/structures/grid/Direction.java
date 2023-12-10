b52ea2ef869a4c2649cfd9a372e100be77be9168
emiddleton <emiddleton@tripadvisor.com>
2023-12-10 20:03:00 +0000

    More direction utils

--- src/esm/aoc/structures/grid/Direction.java
+++ src/esm/aoc/structures/grid/Direction.java
@@ -1,5 +1,7 @@
 package esm.aoc.structures.grid;
 
+import java.util.List;
+
 public enum Direction {
     UP,
     DOWN,
@@ -8,5 +10,26 @@ public enum Direction {
     UP_LEFT,
     UP_RIGHT,
     DOWN_LEFT,
-    DOWN_RIGHT
+    DOWN_RIGHT;
+
+    public static List<Direction> getValues(final boolean includeDiagonal) {
+        if (includeDiagonal) {
+            return List.of(values());
+        }
+        return List.of(UP, DOWN, LEFT, RIGHT);
+    }
+
+    public Direction opposite() {
+        return switch (this) {
+            case UP -> DOWN;
+            case DOWN -> UP;
+            case LEFT -> RIGHT;
+            case RIGHT -> LEFT;
+            case UP_LEFT -> DOWN_RIGHT;
+            case UP_RIGHT -> DOWN_LEFT;
+            case DOWN_LEFT -> UP_RIGHT;
+            case DOWN_RIGHT -> UP_LEFT;
+        };
+    }
+
 }
