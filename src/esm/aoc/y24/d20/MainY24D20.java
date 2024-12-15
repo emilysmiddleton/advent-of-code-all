5324e65c6355b6847633519404402e7a597c5e82
emiddleton <emiddleton@tripadvisor.com>
2024-12-15 21:51:35 +0000

    2024 day placeholders

---
+++ src/esm/aoc/y24/d20/MainY24D20.java
@@ -0,0 +1,21 @@
+package esm.aoc.y24.d20;
+
+import esm.aoc.input.InputReader;
+
+public class MainY24D20 {
+
+    public static void main(String[] args) throws InterruptedException {
+        final var input = InputReader.readStrings("2024/day20.txt");
+        System.out.println(STR."Part 1: \{solve1()}");
+        System.out.println(STR."Part 2: \{solve2()}");
+    }
+
+    private static long solve1() {
+        return 0;
+    }
+
+    private static long solve2() {
+        return 0;
+    }
+
+}
