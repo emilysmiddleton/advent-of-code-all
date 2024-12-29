7baba828465af61a05bd64c3454724cfdaf4ce4e
emiddleton <emiddleton@tripadvisor.com>
2024-12-29 12:17:13 +0000

    Refactor to use the pathfinder

--- src/esm/aoc/y24/d20/MainY24D20.java
+++ src/esm/aoc/y24/d20/MainY24D20.java
@@ -1,25 +1,17 @@
 package esm.aoc.y24.d20;
 
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.function.Predicate;
 
 import esm.aoc.input.InputReader;
-import esm.aoc.structures.collections.Utils;
 import esm.aoc.structures.grid.CharGrid;
 import esm.aoc.structures.grid.Coordinate;
 import esm.aoc.structures.grid.Direction;
-import esm.aoc.structures.grid.tree.GridState;
-import esm.aoc.structures.grid.tree.GridTreeNode;
-import esm.aoc.structures.tree.DepthFirstIterator;
+import esm.aoc.structures.grid.tree.Pathfinder;
 
 public class MainY24D20 {
+
+    private static final int MIN_SAVING = 100;
 
     public static void main(String[] args) throws InterruptedException {
         final var grid = InputReader.readCharGrid("2024/day20.txt");
@@ -30,23 +22,16 @@ public class MainY24D20 {
     private static long solve1(final CharGrid grid) {
         final var path = getPath(grid);
         long count = 0;
-        for (final var cheat : grid.filterByValue("#"::equals)) {
-            // Horizontal
-            final var left = cheat.move(Direction.LEFT);
-            final var right = cheat.move(Direction.RIGHT);
-            if (path.contains(left) && path.contains(right)) {
-                int save = Math.abs(path.indexOf(right) - path.indexOf(left)) - 2;
-                if (save >= 100) {
-                    count++;
-                }
-            }
-            // Vertical
-            final var up = cheat.move(Direction.UP);
-            final var down = cheat.move(Direction.DOWN);
-            if (path.contains(up) && path.contains(down)) {
-                int save = Math.abs(path.indexOf(up) - path.indexOf(down)) - 2;
-                if (save >= 100) {
-                    count++;
+        for (int i = 0; i < path.size(); i++) {
+            final var coordinate = path.get(i);
+            for (final var direction : Direction.getValues(false)) {
+                final var one = coordinate.move(direction);
+                final var two = one.move(direction);
+                if ("#".equals(grid.get(one)) && path.contains(two)) {
+                    final var saving = path.indexOf(two) - i - 2;
+                    if (saving >= MIN_SAVING) {
+                        count++;
+                    }
                 }
             }
         }
@@ -55,29 +40,9 @@ public class MainY24D20 {
 
     private static List<Coordinate> getPath(final CharGrid grid) {
         Coordinate startPosition = grid.filterByValue("S"::equals).stream().findFirst().orElseThrow();
-        final var rootState = getRootState(grid, startPosition);
-        final var root = new GridTreeNode(rootState);
-        final var iterator = new DepthFirstIterator<>(root);
-        final Set<List<Coordinate>> paths = new LinkedHashSet<>();
-        while (iterator.hasNext()) {
-            final GridState next = iterator.next().state();
-            if ("E".equals(next.getValue())) {
-                return next.getPath();
-            }
-        }
-        throw new IllegalStateException();
-    }
-
-    private static GridState getRootState(final CharGrid grid, final Coordinate startPosition) {
-        Predicate<GridState> canMove = state -> {
-            var position = state.getPosition();
-            if (state.getPath().indexOf(position) < state.getPath().size() - 1) {
-                return false;
-            }
-            var value = grid.get(position);
-            return (".".equals(value) || "E".equals(value));
-        };
-        return new GridState(grid, startPosition, canMove, Utils.addAndCopy(Collections.emptyList(), startPosition));
+        Coordinate endPosition = grid.filterByValue("E"::equals).stream().findFirst().orElseThrow();
+        final var pathFinder = new Pathfinder(grid, startPosition, Collections.singleton("."), endPosition::equals);
+        return pathFinder.findPath(Integer.MAX_VALUE).orElseThrow();
     }
 
     private static long solve2() {
