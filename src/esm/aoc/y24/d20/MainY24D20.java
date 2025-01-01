6acf9acf24e70c77617e7de8534330a9551272ef
emiddleton <emiddleton@tripadvisor.com>
2025-01-01 15:51:22 +0000

    2024 day 20

--- src/esm/aoc/y24/d20/MainY24D20.java
+++ src/esm/aoc/y24/d20/MainY24D20.java
@@ -1,41 +1,55 @@
 package esm.aoc.y24.d20;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import esm.aoc.input.InputReader;
 import esm.aoc.structures.grid.CharGrid;
 import esm.aoc.structures.grid.Coordinate;
-import esm.aoc.structures.grid.Direction;
 import esm.aoc.structures.grid.tree.Pathfinder;
 
 public class MainY24D20 {
 
-    private static final int MIN_SAVING = 100;
-
     public static void main(String[] args) throws InterruptedException {
         final var grid = InputReader.readCharGrid("2024/day20.txt");
-        System.out.println(STR."Part 1: \{solve1(grid)}");
-        System.out.println(STR."Part 2: \{solve2()}");
+        System.out.println(STR."Part 1: \{solve(grid, 2)}");
+        System.out.println(STR."Part 2: \{solve(grid, 20)}");
     }
 
-    private static long solve1(final CharGrid grid) {
+    private static long solve(final CharGrid grid, final int limit) {
         final var path = getPath(grid);
+        final var pathCoordinates = new LinkedHashSet<>(path);
         long count = 0;
-        for (int i = 0; i < path.size(); i++) {
-            final var coordinate = path.get(i);
-            for (final var direction : Direction.getValues(false)) {
-                final var one = coordinate.move(direction);
-                final var two = one.move(direction);
-                if ("#".equals(grid.get(one)) && path.contains(two)) {
-                    final var saving = path.indexOf(two) - i - 2;
-                    if (saving >= MIN_SAVING) {
-                        count++;
-                    }
+        final List<String> cheats = new ArrayList<>();
+        for (final var from : path) {
+            for (final var to : getReachable(pathCoordinates, from, limit)) {
+                final var saved = path.indexOf(to) - path.indexOf(from) - (int) from.distance(to);
+                if (saved >= 100) {
+                    count++;
+                }
+            }
+            System.out.println(from + " " + count);
+        }
+        Collections.sort(cheats);
+        cheats.forEach(System.out::println);
+        return count;
+    }
+
+    private static Set<Coordinate> getReachable(final Set<Coordinate> path, final Coordinate from, final int limit) {
+        final Set<Coordinate> reachable = new LinkedHashSet<>();
+        for (int i = (int) (from.x() - limit); i <= from.x() + limit; i++) {
+            for (int j = (int) (from.y() - limit); j <= from.y() + limit; j++) {
+                final var to = new Coordinate(i, j);
+                final var distance = from.distance(to);
+                if (path.contains(to) && distance >= 2 && distance <= limit) {
+                    reachable.add(to);
                 }
             }
         }
-        return count;
+        return reachable;
     }
 
     private static List<Coordinate> getPath(final CharGrid grid) {
@@ -45,8 +59,5 @@ public class MainY24D20 {
         return pathFinder.findPath(Integer.MAX_VALUE).orElseThrow();
     }
 
-    private static long solve2() {
-        return 0;
-    }
-
 }
+
